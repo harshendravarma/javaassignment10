dfc4c088c29c31c8c977b8e9b6a513e2dfa85bb2
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-09-18 12:15:39 +0530

    added output comments

--- Main.java
+++ Main.java
@@ -6,13 +6,13 @@ public class Main {
 		
 		SList list1 = new SList();
 		SListIterator siterator = list1.iterator();
-		System.out.println(list1);
+		System.out.println(list1); 				//list is empty
 		siterator.insert("1");
 		siterator.insert("2");
-		System.out.println(list1);
+		System.out.println(list1);               //SList is  [1, 2]
 		SListIterator siterator2 = list1.iterator();
 		siterator2.remove();
-		System.out.println(list1);
+		System.out.println(list1);    			//SList is  [2]
 
 	}
 
